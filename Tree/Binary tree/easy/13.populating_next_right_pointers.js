f279d9ef96487319736b84f265ead70e9d429a4d
*vivek <vrvivekofficial@gmail.com>
2025-10-24 10:42:21 +0530

    completed

---
+++ Tree/Binary tree/easy/13.populating_next_right_pointers.js
@@ -0,0 +1,33 @@
+/**
+ * // Definition for a _Node.
+ * function _Node(val, left, right, next) {
+ *    this.val = val === undefined ? null : val;
+ *    this.left = left === undefined ? null : left;
+ *    this.right = right === undefined ? null : right;
+ *    this.next = next === undefined ? null : next;
+ * };
+ */
+
+/**
+ * @param {_Node} root
+ * @return {_Node}
+ */
+var connect = function (root) {
+  if (!root) return null;
+  let leftMost = root;
+
+  while (leftMost.left) {
+    let current = leftMost;
+
+    while (current) {
+      current.left.next = current.right;
+
+      if (current.next) {
+        current.right.next = current.next.left;
+      }
+      current = current.next;
+    }
+    leftMost = leftMost.left;
+  }
+  return root;
+};
---
+++ Tree/Binary tree/easy/14.binary_tree_maximum_path.js
@@ -0,0 +1,22 @@
+var maxPathSum = function(root) {
+    let res = root.val;
+
+    function dfs(node) {
+        if (!node) {
+            return 0;
+        }
+
+        // Recursively compute the maximum sum of the left and right subtree paths.
+        let leftSum = Math.max(0, dfs(node.left));
+        let rightSum = Math.max(0, dfs(node.right));
+
+        // Update the maximum path sum encountered so far (with split).
+        res = Math.max(res, leftSum + rightSum + node.val);
+
+        // Return the maximum sum of the path (without split).
+        return Math.max(leftSum, rightSum) + node.val;
+    }
+
+    dfs(root);
+    return res;
+};
