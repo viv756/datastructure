e3db8e0022fe80301068a1a3120ff216409a53e9
*vivek <vrvivekofficial@gmail.com>
2025-11-10 21:25:28 +0530

    *

---
+++ String/12.Find the Index of the First Occurrence in a String.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
+  const h = haystack.length;
+  const n = needle.length;
+
+  for (let i = 0; i <= h - n; i++) {
+    let j = 0;
+    while (j < n) {
+      if (haystack[i + j] !== needle[j]) {
+        break;
+      }
+      j++;
+    }
+    if (j === n) return i;
+  }
+
+  return -1;
+};
--- String/2.find_word_containing.js
+++ String/2.find_word_containing.js
@@ -15,3 +15,4 @@ var findWordsContaining = function (words, x) {
 
   return res;
 };
+
