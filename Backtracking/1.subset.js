628f83b7aa2943fbf0b7574897d96845989844ff
*vivek <vrvivekofficial@gmail.com>
2025-11-20 22:16:00 +0530

    *

--- Backtracking/1.subset.js
+++ Backtracking/1.subset.js
@@ -1,15 +1,18 @@
-var subset = (nums)=>{
-  let res = []
-  let backTrack = (path, start) => {
-    result.push([...path])
-    
-    for (let i = start; i < nums.length; i++){
-      path.push(arr[i])
-      backTrack(path, i + 1)
-      path.pop()
+var subsets = function (nums) {
+  let res = [];
+
+  const backTrack = (path, start) => {
+    res.push([...path]);
+    for (let i = start; i < nums.length; i++) {
+      path.push(nums[i]);
+      console.log(path);
+
+      backTrack(path, i + 1);
+      path.pop();
     }
-  }
-  
-  backTrack([], 0)
-  return res
-}
+  };
+  backTrack([], 0);
+  return res;
+};
+
+subsets([1, 2, 3]);
--- Backtracking/6.combinationSum2.js
+++ Backtracking/6.combinationSum2.js
@@ -14,6 +14,7 @@ var combinationSum2 = function (candidates, target) {
 
     if (remainingSum <= 0) return;
 
+    // because of the array is sorted candidates[i] <= remainingSum
     for (let i = start; i < candidates.length && candidates[i] <= remainingSum; i++) {
       if (i > start && candidates[i] === candidates[i - 1]) {
         continue;
